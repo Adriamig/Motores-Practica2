c89bba875d66c25a4a00772a225185f8eabe26f9
Adrián Migueles <adriamig@ucm.es>
2020-04-16 23:16:55 +0200

    Punto 1 y 2 hecho, la mitad del 3 también

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D rb;
+    private float speedy, speedx;
+    Vector2 velocidad;
+    private bool jugando;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.isKinematic = true;
+        velocidad = new Vector2(0, 0);
+        speedy = speedx = speed;
+        jugando = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return) && jugando)
+        {
+            rb.isKinematic = false;
+
+            transform.SetParent(null);
+
+            velocidad = new Vector2(0, speedy);
+
+            jugando = false;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (!jugando) rb.AddForce(velocidad);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Vector2 movimiento;
+    public float velocityScale;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        float move = Input.GetAxis("Horizontal");
+
+        movimiento = new Vector2(move, 0);
+
+        movimiento.Normalize();
+
+        movimiento *= velocityScale;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = movimiento;
+    }
+}
